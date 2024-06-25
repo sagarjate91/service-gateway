d790ad7bc4f9b7a42011976a84562b31b51af6e8
Sagar Jate <jatekomal@gmail.com>
2024-06-25 12:25:27 +0530

    first commit

---
+++ src/main/java/com/training/service_gateway/ServiceGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.training.service_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGatewayApplication.class, args);
+	}
+
+}
